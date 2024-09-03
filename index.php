89565518ac059e313b0a337170dadf932fe80bf0
Lily-ofthevalley <lilithvdplas@gmail.com>
2024-09-03 14:28:22 +0200

    header and side bar

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Lilith" />
+        <link rel="icon" type="image/x-icon" href="Images/favicon.ico" />
+        <title>To-Do</title>
+        <link href="Styles/style.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    </head>
+    <body>
+
+    <!-- Header -->
+    <header>
+        <nav class="header-container">
+            <div class="header-name">
+                <img src="Images/checklist.png" alt="Logo" id="header-logo">
+                <p>To-Do</p>
+            </div>
+            <a id="header-login">Sign-up/Login</a>
+        </nav>
+    </header>
+
+    <!-- Side bar -->
+    <div class="sideBar-container">
+            <a class="sideBar-text" id="sideBar-ToDo" href="">To-Do</a>
+            <a class="sideBar-text" id="sideBar-finished" href="">Finished</a>
+            <p class="sideBar-text" id="sideBar-addTask">Add Task</p>
+    </div>
+
+    <!-- Login/Sign-Up -->
+
+
+    <!-- Add Task -->
+
+
+    </body>
+</html>
